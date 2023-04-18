0a75161726f1e0747b9545837d5b1c65f50cc29e
Dev8446 <124416882+Dev8446@users.noreply.github.com>
2023-04-18 15:44:43 +0530

    Home Problem
    Home Hover effect problem solved

--- FlexStart/index.php
+++ FlexStart/index.php
@@ -1056,6 +1056,23 @@ include 'navbar.php';
         </div>
 
       </div>
+      <script>
+        <script>
+    const links = document.querySelectorAll('.nav-link');
+    
+    if (links.length) {
+      links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+          links.forEach((link) => {
+              link.classList.remove('active');
+          });
+          e.preventDefault();
+          link.classList.add('active');
+        });
+      });
+    }
+  </script>
+      </script>
 
     </section><!-- End Recent Blog Posts Section -->
 
